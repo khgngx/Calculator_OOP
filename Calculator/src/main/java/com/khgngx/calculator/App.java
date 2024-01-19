195c8d39acaf4478dca3c80a090b79020d230a07
Nguyen Van Khang <144866460+khgngx@users.noreply.github.com>
2024-01-19 17:42:57 +0700

    Add files via upload

---
+++ Calculator/src/main/java/com/khgngx/calculator/App.java
@@ -0,0 +1,18 @@
+package com.khgngx.calculator;
+
+import com.khgngx.calculator.UI.CalculatorUI;
+
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+
+        ImageIcon icon = new ImageIcon("\"C:\\Users\\ADMIN\\Downloads\\z4907742248387_12cc9e75d84a0248ab4897dd76b8c5f6.jpg\"");
+
+
+    }
+}
+
+
---
+++ Calculator/src/main/java/com/khgngx/calculator/Theme/Properties/ThemeList.java
@@ -0,0 +1,23 @@
+package com.khgngx.calculator.Theme.Properties;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ThemeList {
+    private List<Theme> themes;
+
+
+    public List<Theme> getThemes() {
+        return themes;
+    }
+
+    public void setThemes(List<Theme> themes) {
+        this.themes = themes;
+    }
+
+    public Map<String, Theme> getThemesAsMap() {
+        return themes.stream().collect(Collectors.toMap(Theme::getName, Function.identity()));
+    }
+}
